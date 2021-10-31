2c82080a5cd991f2ed1e27002c396eb736c487b6
Mr. Paul Ishaili <paulishaili@gmail.com>
2021-10-31 04:29:41 +0100

    Populating content with ejs

--- app.js
+++ app.js
@@ -16,5 +16,55 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  const furnitures = [
+    {
+      name: "Furniture 1",
+      description:
+        "a brief entitled description of the newly made furniture 1 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 2",
+      description:
+        "a brief entitled description of the newly made furniture 2 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 3",
+      description:
+        "a brief entitled description of the newly made furniture 3 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 4",
+      description:
+        "a brief entitled description of the newly made furniture 4 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 5",
+      description:
+        "a brief entitled description of the newly made furniture 5 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 6",
+      description:
+        "a brief entitled description of the newly made furniture 6 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 7",
+      description:
+        "a brief entitled description of the newly made furniture 7 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+    {
+      name: "Furniture 8",
+      description:
+        "a brief entitled description of the newly made furniture 8 for all and sundry",
+      image: "/images/pexels-photo-1866149.jpeg",
+    },
+  ];
+  res.render("index", { furnitures: furnitures });
 });
