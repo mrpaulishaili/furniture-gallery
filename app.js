e11824b3a1280f1b03baf39ceabc3a4e13989b7b
Mr. Paul Ishaili C <paulishaili@gmail.com>
2021-12-13 16:13:50 +0100

    Update new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Listen to PORT
 app.listen(PORT, () => {
